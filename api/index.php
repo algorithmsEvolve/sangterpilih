ca3a84bfdce35ddeb547f3a16f84ff5ee30a7a36
Algorithm <49584601+algorithmsEvolve@users.noreply.github.com>
2026-04-03 00:16:08 +0700

    fix: deploy problem #6

--- api/index.php
+++ api/index.php
@@ -4,7 +4,14 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
 // Force temp storage for Vercel's read-only filesystem
-$app->useStoragePath('/tmp/storage');
+$storagePath = '/tmp/storage';
+if (!is_dir($storagePath)) {
+    mkdir($storagePath . '/framework/cache/data', 0777, true);
+    mkdir($storagePath . '/framework/sessions', 0777, true);
+    mkdir($storagePath . '/framework/views', 0777, true);
+    mkdir($storagePath . '/logs', 0777, true);
+}
+$app->useStoragePath($storagePath);
 // $app->useBootstrapPath('/tmp/bootstrap'); 
 
 $app->handleRequest(Illuminate\Http\Request::capture());
