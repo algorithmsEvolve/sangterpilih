e10975ebdb2d0824a3092b6b456383fe6406db53
Algorithm <49584601+algorithmsEvolve@users.noreply.github.com>
2026-04-03 00:09:03 +0700

    fix: deploy problem #5

--- api/index.php
+++ api/index.php
@@ -1,3 +1,10 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Force temp storage for Vercel's read-only filesystem
+$app->useStoragePath('/tmp/storage');
+// $app->useBootstrapPath('/tmp/bootstrap'); 
+
+$app->handleRequest(Illuminate\Http\Request::capture());
